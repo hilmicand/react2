4e76a9a57bd80d1a5dcabed3be7dc28a6a349306
hilmicand <hilmicandelibas97@gmail.com>
2018-11-06 15:43:19 +0300

    Created

--- src/App.js
+++ src/App.js
@@ -4,23 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var baslik = React.createElement('h1', {className: "baslik"}, 'Başlık');
+    var content = React.createElement('div', {className: "baslik_"}, [baslik]);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      content
     );
   }
 }
